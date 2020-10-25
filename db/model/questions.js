3173c342c5069590bba518dfcd776001f91ef7a7
Chandan Kumar <chandan.kumar9503@gmail.com>
2020-10-25 12:26:01 +0000

    DB models and operations

---
+++ db/model/questions.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const questions = {
+  name : 'questions',
+  attributes : ['id', 'test_id', 'section', 'sub_section', 'question', 'a', 'b', 'c', 'd', 'answerKey'],
+};
+
+module.exports = questions;
---
+++ db/model/tests.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const tests = {
+  name : 'tests',
+  attributes : ['id', 'section'],
+};
+
+module.exports = tests;
