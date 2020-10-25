865f6cf477a85a5705b9e0024748a336d667aebf
Chandan Kumar <chandan.kumar9503@gmail.com>
2020-10-25 13:06:56 +0000

    error codes for invalid request

--- constants/errors.js
+++ constants/errors.js
@@ -2,6 +2,9 @@
 
 const errors = {
   GE_4000: {name: 'GE_4000', message: 'Something went wrong. Please try again after some time.', status: 400},
+
+  GE_DB_4000: {name: 'GE_DB_4000', message: 'Invalid input, Please try again with valid data.', status: 400},
+  GE_DB_4001: {name: 'GE_DB_4001', message: 'No data found', status: 404},
 };
 
 module.exports = errors;
--- service/testSeries.js
+++ service/testSeries.js
@@ -5,8 +5,10 @@ const util = require('util');
 
 const Questions = require('../db/operation/questions');
 const Tests = require('../db/operation/tests');
+const ApiError = require('../lib/error');
 const harperDbConfig = require('../config').harperDb;
- 
+const errors = require('../constants/errors');
+
 const Client = harperive.Client;
 const client = new Client(harperDbConfig);
 const questions = new Questions(client);
@@ -16,40 +18,85 @@ const testSeries = {
   getAllTests: function(){
     return new Promise((resolve, reject) => {
       tests.getAll()
-        .then((resp) => resolve(resp))
-        .catch((err) => reject(err));
+        .then((resp) => {
+          if (resp.length === 0) {
+            util.log('[GET ALL TEST] :: No Tests Found');
+            return reject(new ApiError(errors.GE_DB_4001.status, errors.GE_DB_4001.message, errors.GE_DB_4001.name));
+          }
+          return resolve(resp);
+        })
+        .catch((err) =>  {
+          util.log('[GET ALL TEST] :: error occurred :: ', err);
+          return reject(new ApiError(errors.GE_DB_4000.status, errors.GE_DB_4000.message, errors.GE_DB_4000.name));
+        });
     });
   },
 
   getQuestionsByTestId: function(testId) {
     return new Promise((resolve, reject) => {
       questions.getByAttribute('test_id', testId)
-        .then((resp) => resolve(resp))
-        .catch((err) => reject(err));
+        .then((resp) => {
+          if (resp.length === 0) {
+            util.log('[GET QUESTIONS BY TEST ID] :: No Questions Found');
+            return reject(new ApiError(errors.GE_DB_4001.status, errors.GE_DB_4001.message, errors.GE_DB_4001.name));
+          }
+          return resolve(resp);
+        })
+        .catch((err) =>  {
+          util.log('[GET QUESTIONS BY TEST ID] :: error occurred :: ', err);
+          return reject(new ApiError(errors.GE_DB_4000.status, errors.GE_DB_4000.message, errors.GE_DB_4000.name));
+        });
     });
   },
 
   getQuestionsBySection: function(section) {
     return new Promise((resolve, reject) => {
       questions.getByAttribute('section', section)
-        .then((resp) => resolve(resp))
-        .catch((err) => reject(err));
+        .then((resp) => {
+          if (resp.length === 0) {
+            util.log('[GET QUESTIONS BY SECTION] :: No Questions Found');
+            return reject(new ApiError(errors.GE_DB_4001.status, errors.GE_DB_4001.message, errors.GE_DB_4001.name));
+          }
+          return resolve(resp);
+        })
+        .catch((err) =>  {
+          util.log('[GET QUESTIONS BY SECTION] :: error occurred :: ', err);
+          return reject(new ApiError(errors.GE_DB_4000.status, errors.GE_DB_4000.message, errors.GE_DB_4000.name));
+        });
     });
   },
 
   getQuestionsBySubSection: function(subSection) {
     return new Promise((resolve, reject) => {
       questions.getByAttribute('sub_section', subSection)
-        .then((resp) => resolve(resp))
-        .catch((err) => reject(err));
+        .then((resp) => {
+          if (resp.length === 0) {
+            util.log('[GET QUESTIONS BY SUBSECTION] :: No Questions Found');
+            return reject(new ApiError(errors.GE_DB_4001.status, errors.GE_DB_4001.message, errors.GE_DB_4001.name));
+          }
+          return resolve(resp);
+        })
+        .catch((err) =>  {
+          util.log('[GET QUESTIONS BY SUBSECTION] :: error occurred :: ', err);
+          return reject(new ApiError(errors.GE_DB_4000.status, errors.GE_DB_4000.message, errors.GE_DB_4000.name));
+        });
     });
   },
 
   getAllQuestions: function(){
     return new Promise((resolve, reject) => {
       questions.getAll()
-        .then((resp) => resolve(resp))
-        .catch((err) => reject(err));
+        .then((resp) => {
+          if (resp.length === 0) {
+            util.log('[GET ALL QUESTIONS] :: No Questions Found');
+            return reject(new ApiError(errors.GE_DB_4001.status, errors.GE_DB_4001.message, errors.GE_DB_4001.name));
+          }
+          return resolve(resp);
+        })
+        .catch((err) =>  {
+          util.log('[GET ALL QUESTIONS] :: error occurred  :: ', err);
+          return reject(new ApiError(errors.GE_DB_4000.status, errors.GE_DB_4000.message, errors.GE_DB_4000.name));
+        });
     });
   },
 
@@ -58,7 +105,10 @@ const testSeries = {
     return new Promise((resolve, reject) => {
       questions.save(data)
         .then((resp) => resolve(resp))
-        .catch((err) => reject(err));
+        .catch((err) =>  {
+          util.log('[ADD QUESTIONS] :: error occured :: ', err);
+          return reject(new ApiError(errors.GE_DB_4000.status, errors.GE_DB_4000.message, errors.GE_DB_4000.name));
+        });
     });
   }
 };
